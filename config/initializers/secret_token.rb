439941f60bb5d949299c60a698aea101fcc3f7e2
Jeremy <djreyes@gmail.com>
2012-07-30 10:34:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = '60dd48d9f5710854267f4a37c4bb4df1f16a489e6b8919bcde333df9a480a41232d08489f928b46ecab8c7cbe505237220b88319b28fd433cbad91016c196473'
