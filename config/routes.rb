a694e6466bbdd63d8fe841a9ede3bba5c1e8d366
Jeremy <djreyes@gmail.com>
2012-07-30 16:39:53 -0700

    generate response controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Pollster::Application.routes.draw do
+  get "responses/index"
+
+  get "responses/show"
+
+  get "responses/new"
+
+  get "responses/create"
+
+  get "responses/edit"
+
+  get "responses/update"
+
+  get "responses/destroy"
+
   root :to => "polls#index"
 
   resources :polls do
